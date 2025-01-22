f4ba88a936c9bb303390dffbc6da976d9ab182bb
JosueNieves0702 <123047834@upq.edu.mx>
2025-01-22 14:42:26 -0600

    estructura de un archivo java y manej de cadenas

--- Sintaxis/src/main1.java
+++ Sintaxis/src/main1.java
@@ -1,6 +1,18 @@
+import java.util.*;
+//Clase principal
 public class main1 {
+    //Metodo principal
     public static void main(String[] args) throws Exception {
+        //2. cadenas de datos.
         System.out.println("Hello, World!");
         System.out.println("Mi nombre es Joaquin");
-    }
+        String cadenas = "Joaquin " + "Josue " + "Moreno " + "Nieves";
+        System.out.println(cadenas);
+        System.out.println(cadenas.length());
+        System.out.println(cadenas.substring(2, 5));
+        System.out.println(cadenas.substring(2));
+        System.out.println(cadenas.substring(0, 5));
+
+
+    }   
 }
