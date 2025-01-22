ea9e984bc93c7182b2433c78a0ad08491802dc28
JosueNieves0702 <123047834@upq.edu.mx>
2025-01-22 14:11:06 -0600

    Se creo el proyecto

---
+++ Sintaxis/src/main1.java
@@ -0,0 +1,6 @@
+public class main1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Mi nombre es Joaquin");
+    }
+}
