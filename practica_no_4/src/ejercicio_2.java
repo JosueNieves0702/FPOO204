a2e5bd281328182d74d63067440887a97390f40b
JosueNieves0702 <123047834@upq.edu.mx>
2025-01-28 16:09:15 -0600

    ejercicio no 3

---
+++ practica_no_4/src/ejercicio_2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ejercicio_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce un número entero: ");
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("El número es par.");
+        } else {
+            System.out.println("El número es impar.");
+        }
+    }
+}
