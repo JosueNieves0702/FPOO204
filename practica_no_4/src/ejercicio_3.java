160d2c260a74d41eca858295a09371e248f3549d
JosueNieves0702 <123047834@upq.edu.mx>
2025-01-28 16:44:10 -0600

    correccion ejercicio no 3

--- practica_no_4/src/ejercicio_3.java
+++ practica_no_4/src/ejercicio_3.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 
 public class ejercicio_3 {
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Bienvenido a Recorcholos :)\nPor favor, ingrese su edad:");
-    int edad = sc.nextInt();
-    if (edad<=4) {
-        System.out.println("Adelante, su pase es GRATUITO.");
-    
-    } else if (edad > 4 && edad < 18) {
-        
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bienvenido a Recorcholos :)\nPor favor, ingrese su edad:");
+        int edad = sc.nextInt();
+
+        if (edad <= 4) {
+            System.out.println("Adelante, su pase es GRATUITO.");
+        } else if (edad > 4 && edad < 18) {
+            System.out.println("El precio de la entrada es $110.");
+        } else if (edad >= 18) {
+            System.out.println("El precio de la entrada es $190.");
+        } else {
+            System.out.println("Edad inválida.");
+        }
+
+        sc.close();
     }
-else if (edad >= 18) {
-    System.out.println("El precio de la entrada es $190.");
-} else {
-    System.out.println("El precio de la entrada es $110.");
-}
-}
 }
