d71cd8bf4ac05b192bdf66e7daafdcec4155555e
JosueNieves0702 <123047834@upq.edu.mx>
2025-01-28 16:38:33 -0600

    ejercicio no 5

---
+++ practica_no_4/src/ejercicio_5.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ejercicio_5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el primer número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Ingrese el segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Ingrese un operador (+, -, *, /): ");
+        char operador = scanner.next().charAt(0);
+
+        double resultado;
+
+        switch (operador) {
+            case '+':
+                resultado = num1 + num2;
+                System.out.println("El resultado es: " + resultado);
+                break;
+            case '-':
+                resultado = num1 - num2;
+                System.out.println("El resultado es: " + resultado);
+                break;
+            case '*':
+                resultado = num1 * num2;
+                System.out.println("El resultado es: " + resultado);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                    System.out.println("El resultado es: " + resultado);
+                } else {
+                    System.out.println("Error: División por cero.");
+                }
+                break;
+            default:
+                System.out.println("Error: Operador no válido.");
+                break;
+        }
+
+        scanner.close();
+    }
+}
