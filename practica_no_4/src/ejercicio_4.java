ce07d8f8517c20fd53ec6c7384cb83865edb430e
JosueNieves0702 <123047834@upq.edu.mx>
2025-01-28 16:50:37 -0600

    Correccion ejercicio no 4

--- practica_no_4/src/ejercicio_4.java
+++ practica_no_4/src/ejercicio_4.java
@@ -1,32 +1,27 @@
-import java.util.*;
+import java.util.Scanner;
 
-    public class ejercicio_4 {
-        public static void main (String[]args) {
-        
-        
-        Scanner sc = new Scanner (System.in);
-        
+public class ejercicio_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Introduce una palabra: ");
-        
         String cadena_texto = sc.nextLine().toLowerCase();
-        
-        
-        for (int i=0 ; i<cadena_texto.length() ; i++ ) {
-            
-            for (int j = cadena_texto.length() ; j>0 ; j--) {
-                
-                if (cadena_texto.charAt(i) == cadena_texto.charAt(j)) {
-                    
-                    System.out.println("La palabra es palindroma");
-                    
-                }
-                
-                else { System.out.println("La palabra no es palindroma");
-                    
-                }
-                
+        boolean esPalindromo = true;
+        int longitud = cadena_texto.length();
+
+        for (int i = 0; i < longitud / 2; i++) {
+            if (cadena_texto.charAt(i) != cadena_texto.charAt(longitud - i - 1)) {
+                esPalindromo = false;
+                break;
             }
-            
         }
+
+        if (esPalindromo) {
+            System.out.println("La palabra es palíndroma.");
+        } else {
+            System.out.println("La palabra no es palíndroma.");
+        }
+
+        sc.close();
     }
 }
