e9578f13e084c5a6f29471d695f1b4a16d54bdb8
JosueNieves0702 <123047834@upq.edu.mx>
2025-01-29 15:27:21 -0600

    corrección nombre ejercicio no4

--- Practica_no5/src/ejercicio_4.java
+++ Practica_no5/src/ejercicio_4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TrianguloImpares {
+public class ejercicio_4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
